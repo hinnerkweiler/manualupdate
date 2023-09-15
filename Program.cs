1e4bd7163b007cc827271540c1b26ff8099a18e6
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-09-15 17:50:10 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -7,13 +7,28 @@ using Appwrite.Extensions;
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
 using System.Collections;
+using System.Xml.Schema;
 
 public class Handler {
+
+    string apikey = Environment.GetEnvironmentVariable("APPWRITE_KEY");
+    string endpoint = Environment.GetEnvironmentVariable("APPWRITE_ENDPOINT");
+    string projectId = Environment.GetEnvironmentVariable("APPWRITE_FUNCTION_PROJECT_ID");
+
+    Client client = new Client();
+        
 
     // This is your Appwrite function
     // It is executed each time we get a request
     public async Task<RuntimeOutput> Main(RuntimeContext Context) 
     {
+        //initialize the client
+        client
+            .SetEndpoint(endpoint)
+            .SetProject(projectId)
+            .SetKey(apikey)
+        ;
+
         // You can log messages to the console
         // if environment variable LOG_REQUESTS is set to true logs the request details
         if (Environment.GetEnvironmentVariable("LOG_REQUESTS") == "true") {
@@ -44,10 +59,71 @@ public class Handler {
     }
     private async Task<Dictionary<string, object?>> Post(DotNetRuntime.RuntimeContext Context)
     {
-        return new Dictionary<string, object>()
+        var body = JsonSerializer.Serialize<object>(Context.Req.Body);
+        string fileId = ""; 
+        DateTime current = DateTime.UtcNow;
+
+        // Validate that body holds a json object
+        if (!body.StartsWith("{")) {
+            throw new Exception("Invalid JSON object");
+        }
+
+        // Parse the body into a dictionary
+        var data = JsonSerializer.Deserialize<Dictionary<string, object>>(body);
+
+        if (data.ContainsKey("file") && (data["file"] is string)) {
+            if (data.ContainsKey("current")) {
+                try {
+                    current = DateTime.Parse(data["current"].ToString());
+                }
+                catch (Exception e) {
+                    throw new Exception("Invalid date format");
+                }
+            }
+            else {
+                throw new Exception("Missing date");
+            }
+            fileId = data["file"].ToString();
+        }
+        else {
+            throw new Exception("Invalid file");
+        }
+
+        if (fileId == "") {
+            throw new Exception("Invalid file");
+        }
+
+        if (await ServerFileIsNewer(fileId, current))
+        {
+            return new Dictionary<string, object>()
                     {
-                        { "message", "This is not the endpoint you were looking for!" },
-                        { "timestamp", DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff zzz") }
+                        { "update", GetFileUrl(fileId) }
                     };
+        }
+        else {
+            return new Dictionary<string, object>()
+                    {
+                        { "url", "no" },
+                    };
+        }
     }
-}
+
+    private async Task<bool> ServerFileIsNewer(string fileId, DateTime current) {
+
+        var storage = new Storage(client);
+        var file = await storage.GetFile("pub", fileId);
+
+        if (DateTime.Parse(file.UpdatedAt) > current) {
+            return true;
+        }        
+            return false;
+    }
+
+    private string GetFileUrl(string fileId) {
+
+        return "https://api.segeln.socieal/v1/storage/buckets/pub/files/" + fileId + "/download";
+    }
+
+}    
+
+        
